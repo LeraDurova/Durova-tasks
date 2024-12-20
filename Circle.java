a55d82a0233daf9a29de9d248e80e24415a24363
Valeria Durova <lera.durova.06@mail.com>
2024-12-20 16:13:19 +0300

    Durova iu1-12b. shapes

--- Circle.java
+++ Circle.java
@@ -1,19 +1,14 @@
-//public class Circle {
-//    double radius;
-//    public Circle(double radius){
-//        this.radius = radius;
-//    }
-//
-//    public double findPerimeter(){
-//        return 2*3.14*radius;
-//    }
-//
-//    public double findSquare(){
-//        return 3.14*radius*radius;
-//    }
-//}
 public class Circle {
-    public static void main(String[] args)
-    { big_loop: for (int i = 0; i < 3 ; i++)
-    { try { for (int j = 0; j < 3 ; j++)
-    { if (i == j) continue; else if (i > j) continue big_loop; System.out.print("A"); } } finally { System.out.print("B"); } System.out.print("C"); } } }
+    double radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double findPerimeter(){
+        return 2*3.14*radius;
+    }
+
+    public double findSquare(){
+        return 3.14*radius*radius;
+    }
+}
